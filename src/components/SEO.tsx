fd13a81bd1f332409089e729d9d8ab86452f4c1b
rotciv <ngocthang.devweb@gmail.com>
2024-10-31 22:32:24 +0700

    resolve open graph images

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -7,17 +7,14 @@ interface SEOProps {
 }
 
 const SEOConfig = ({ title, description, keywords }: SEOProps) => (
-  <Helmet>
+  <Helmet async={true}>
     <title>{title}</title>
     <meta name="description" content={description} />
     <meta name="keywords" content={keywords} />
     <meta name="robots" content="index, follow" />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
-    <meta
-      property="og:image"
-      content="https://www.thuanhighclean.com/assets/open_graph.png"
-    />
+    <meta property="og:image" content="/assets/open_graph.png" />
     <meta property="og:url" content="https://thuanhighclean.com" />
     <meta property="og:type" content="website" />
   </Helmet>
